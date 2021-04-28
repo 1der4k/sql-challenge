aa69c9aa377d2031df5c60ddc1ea624d452a9988
Zavala <toro.d.c@gmail.com>
2021-04-28 14:37:41 -0400

    updated schemata.sql with code that does NOT include quotes around table and field titles, as well as removed capital letters for convenience

--- EmployeeSQL/schemata.sql
+++ EmployeeSQL/schemata.sql
@@ -1,30 +1,30 @@
-CREATE TABLE "Department" (
-    "dept_no" INT PRIMARY KEY NOT NULL,
-    "dept_name" VARCHAR(55) NOT NULL
+CREATE TABLE department (
+    dept_no INT PRIMARY KEY NOT NULL,
+    dept_name VARCHAR(55) NOT NULL
     );
 
-CREATE TABLE "Title" (
-    "title_id" VARCHAR(5) PRIMARY KEY NOT NULL,
-    "title" VARCHAR(55) NOT NULL
+CREATE TABLE title (
+    title_id VARCHAR(5) PRIMARY KEY NOT NULL,
+    title VARCHAR(55) NOT NULL
 	);
 
-CREATE TABLE "Employee" (
-    "emp_no" INT PRIMARY KEY NOT NULL,
-    "emp_title_id" VARCHAR(55) REFERENCES "Title" ("title_id"),
-    "birth_date" DATE NOT NULL,
-    "first_name" VARCHAR NOT NULL,
-    "last_name" VARCHAR NOT NULL,
-    "sex" VARCHAR(5) NOT NULL,
-    "hire_date" DATE NOT NULL 
+CREATE TABLE employee (
+    emp_no INT PRIMARY KEY NOT NULL,
+    emp_title_id VARCHAR(55) REFERENCES Title (title_id),
+    birth_date DATE NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    sex VARCHAR(5) NOT NULL,
+    hire_date DATE NOT NULL 
 	);
 
-CREATE TABLE "Employee_Dept" (
-    "dept_no" INT REFERENCES "Department" ("dept_no"),
-    "emp_no" INT REFERENCES "Employee" ("emp_no"),
-	PRIMARY KEY("dept_no","emp_no")
+CREATE TABLE employee_Dept (
+    dept_no INT REFERENCES Department (dept_no),
+    emp_no INT REFERENCES Employee (emp_no),
+	PRIMARY KEY(dept_no,emp_no)
 	);
 
-CREATE TABLE "Salary" (
-    "emp_no" INT PRIMARY KEY REFERENCES "Employee" ("emp_no"),
-    "salary" INT NOT NULL
+CREATE TABLE salary (
+    emp_no INT PRIMARY KEY REFERENCES Employee (emp_no),
+    salary INT NOT NULL
 	);
