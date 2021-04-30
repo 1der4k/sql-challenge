74d8b1c0c7d70fa572fe24a9e94dc641e31d4974
Zavala <toro.d.c@gmail.com>
2021-04-30 13:37:02 -0400

    code for selection using left joins to complete task 3

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -1,6 +1,16 @@
 SELECT employee.emp_no,employee.last_name,employee.first_name,employee.sex,salary.salary
 FROM employee
-INNER JOIN salary ON employee.emp_no=salary.emp_no
+INNER JOIN salary
+ON employee.emp_no=salary.emp_no
 
-SELECT first_name,last_name,hire_date FROM employee
+SELECT first_name,last_name,hire_date
+FROM employee
 WHERE hire_date LIKE '%1986'
+
+SELECT dept_manager.dept_no,department.dept_name,dept_manager.emp_no,employee.first_name,employee.last_name
+FROM department
+LEFT JOIN dept_manager
+ON dept_manager.dept_no=department.dept_no
+LEFT JOIN employee
+ON dept_manager.emp_no=employee.emp_no
+
